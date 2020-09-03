4347b2e72d62f7e794c8c380c3659c189452ef44
Kobi Donner <kobibo@gmail.com>
2020-09-03 12:15:20 +0300

    Updates

---
+++ precache-manifest.7e6680d986c7d7f9ba2dfe89738c1b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0c58b7d83df7302ebc89184955dd91",
+    "url": "/yuvalim/index.html"
+  },
+  {
+    "revision": "e54d26154a06b2297d91",
+    "url": "/yuvalim/static/css/main.8abff518.chunk.css"
+  },
+  {
+    "revision": "bfeacad501f3b833b0c6",
+    "url": "/yuvalim/static/js/2.a37a5020.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/yuvalim/static/js/2.a37a5020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54d26154a06b2297d91",
+    "url": "/yuvalim/static/js/main.bcb679c9.chunk.js"
+  },
+  {
+    "revision": "7ecf2e19aa0bdc94e8a9",
+    "url": "/yuvalim/static/js/runtime-main.baf2ff1a.js"
+  }
+]);
